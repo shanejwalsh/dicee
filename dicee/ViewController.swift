796d07a25be9461d50e891474a707cc2a10c4b24
Shane Walsj <shanewalsh@hey.com>
2020-08-06 22:46:04 +0100

    Initial Commit

---
+++ dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Shane Walsh on 06/08/2020.
+//  Copyright © 2020 Shane Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
